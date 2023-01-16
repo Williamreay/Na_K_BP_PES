75b8ebb39daa839ae0a4535232f81fe067ae8ea7
Williamreay <48968264+Williamreay@users.noreply.github.com>
2023-01-16 15:54:44 +1100

    Update Na_K_stage_step_2b_exploring_PES_int_unmed.R
    Changed to per genetic score decile

--- PES_scripts/Na_K_stage_step_2b_exploring_PES_int_unmed.R
+++ PES_scripts/Na_K_stage_step_2b_exploring_PES_int_unmed.R
@@ -47,13 +47,25 @@ PGS_BP <- fread("UKBB_PGS/SBP_DBP_PGS_UKBB_exTrained.txt")
 
 Merged_PGS_PES <- merge(PGS_BP, Merged_PES, by = "IID")
 Merged_PGS_PES$Transport_SBP_PES_AVG <- as.numeric(scale(Merged_PGS_PES$Transport_SBP_PES_AVG))
+Merged_PGS_PES$SBP_PGS_Pt_0_01_AVG <- as.numeric(scale(Merged_PGS_PES$SBP_PGS_Pt_0_01_AVG))
 
-## Test effect of scaled Na:K ratio
-Merged_PGS_PES$Scaled_Na <- as.numeric(scale(Merged_PGS_PES$Na_K_ratio))
+## PESxPRS int - note that display stat sig corr (r = 0.16)
+
+cor.test(Merged_PGS_PES$Transport_SBP_PES_AVG, Merged_PGS_PES$SBP_PGS_Pt_0_01_AVG)
+
+PES_by_PRS <- lm(Mean_SBP ~ Sex*Transport_SBP_PES_AVG + Age*Transport_SBP_PES_AVG + Age2*Transport_SBP_PES_AVG + Assesment_centre_month*Transport_SBP_PES_AVG + 
+                   Assesment_centre*Transport_SBP_PES_AVG + PC1*Transport_SBP_PES_AVG + PC2*Transport_SBP_PES_AVG+ 
+                   PC3*Transport_SBP_PES_AVG + PC4*Transport_SBP_PES_AVG + PC5*Transport_SBP_PES_AVG + PC10*Transport_SBP_PES_AVG+ PC11*Transport_SBP_PES_AVG +
+                   PC12*Transport_SBP_PES_AVG + PC13*Transport_SBP_PES_AVG + PC14*Transport_SBP_PES_AVG + PC15*Transport_SBP_PES_AVG + 
+                   PC16*Transport_SBP_PES_AVG + PC17*Transport_SBP_PES_AVG + PC18*Transport_SBP_PES_AVG + PC19*Transport_SBP_PES_AVG + PC20*Transport_SBP_PES_AVG + 
+                   Transport_SBP_PES_AVG*SBP_PGS_Pt_0_01_AVG, data = Merged_PGS_PES)
+
+## Test effect of scaled Na:K ratio and scaled Na
+Merged_PGS_PES$Scaled_Na <- as.numeric(scale(Merged_PGS_PES$Na_urine_millimolL))
 
 TE <- lm(Mean_SBP ~ Sex + Age + Age2 + Assesment_centre_month + 
   Assesment_centre + PC1 + PC2 + PC3 + PC4 + PC5 + PC10 + PC11 +
-  PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + Transport_SBP_PES_AVG*Scaled_Na, data = Merged_PGS_PES)
+  PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + Scaled_Na*Transport_SBP_PES_AVG, data = Merged_PGS_PES)
 
 Ratio_Transport_int_G_C_C_E <- lm(Mean_SBP ~ Sex*Transport_SBP_PES_AVG + Age*Transport_SBP_PES_AVG + Age2*Transport_SBP_PES_AVG + Assesment_centre_month*Transport_SBP_PES_AVG + 
                               Assesment_centre*Transport_SBP_PES_AVG + PC1*Transport_SBP_PES_AVG + PC2*Transport_SBP_PES_AVG+ 
@@ -68,12 +80,45 @@ Ratio_Transport_int_G_C_C_E <- lm(Mean_SBP ~ Sex*Transport_SBP_PES_AVG + Age*Tra
 
 summary(Ratio_Transport_int_G_C_C_E)
 
+## Same on DBP
+
+DBP_Ratio_Transport_int_G_C_C_E <- lm(Mean_DBP ~ Sex*Transport_SBP_PES_AVG + Age*Transport_SBP_PES_AVG + Age2*Transport_SBP_PES_AVG + Assesment_centre_month*Transport_SBP_PES_AVG + 
+                                    Assesment_centre*Transport_SBP_PES_AVG + PC1*Transport_SBP_PES_AVG + PC2*Transport_SBP_PES_AVG+ 
+                                    PC3*Transport_SBP_PES_AVG + PC4*Transport_SBP_PES_AVG + PC5*Transport_SBP_PES_AVG + PC10*Transport_SBP_PES_AVG+ PC11*Transport_SBP_PES_AVG +
+                                    PC12*Transport_SBP_PES_AVG + PC13*Transport_SBP_PES_AVG + PC14*Transport_SBP_PES_AVG + PC15*Transport_SBP_PES_AVG + 
+                                    PC16*Transport_SBP_PES_AVG + PC17*Transport_SBP_PES_AVG + PC18*Transport_SBP_PES_AVG + PC19*Transport_SBP_PES_AVG + PC20*Transport_SBP_PES_AVG + 
+                                    Scaled_Na*Transport_SBP_PES_AVG +
+                                    Sex*Scaled_Na + Age*Scaled_Na + Age2*Scaled_Na + Assesment_centre_month*Scaled_Na + 
+                                    Assesment_centre*Scaled_Na + PC1*Scaled_Na + PC2*Scaled_Na+ PC3*Scaled_Na + PC4*Scaled_Na + PC5*Scaled_Na + PC10*Scaled_Na+ PC11*Scaled_Na +
+                                    PC12*Scaled_Na + PC13*Scaled_Na + PC14*Scaled_Na + PC15*Scaled_Na + PC16*Scaled_Na + PC17*Scaled_Na + PC18*Scaled_Na + PC19*Scaled_Na + PC20*Scaled_Na,
+                                  data = Merged_PGS_PES)
+
+summary(DBP_Ratio_Transport_int_G_C_C_E)
+
+
+## Calculate heteroskedasticity robust SE (HC3, https://doi.org/10.1080/00031305.2000.10474549)
+## Heteroskedasticity of the residuals can lead to inflation
+
+library(lmtest)
+library(sandwich)
+
+## White's Estimator
+
+coeftest(Ratio_Transport_int_G_C_C_E, vcov = vcovHC(Ratio_Transport_int_G_C_C_E, type="HC0"))
+
+## Transport_SBP_PES_AVG:Scaled_Na                 9.7531e-02  3.7263e-02   2.6174 0.0088609
+
+coeftest(Ratio_Transport_int_G_C_C_E, vcov = vcovHC(Ratio_Transport_int_G_C_C_E, type="HC3"))
+
+## Transport_SBP_PES_AVG:Scaled_Na                 9.7531e-02  3.7445e-02   2.6047 0.0091971
+
 x <- Merged_PGS_PES$Transport_SBP_PES_AVG
+y <- Merged_PGS_PES$SBP_PGS_Pt_0_01_AVG
 
 Merged_PGS_PES$PES <-
-  case_when(x > mean(x)+sd(x) ~ "High",
-            x < mean(x)+sd(x) & x > mean(x)-sd(x) ~ "Average",
-            x < mean(x)-sd(x) ~ "Low")
+  case_when(x > mean(x)+sd(x) ~ "1 SD > Mean",
+            x < mean(x)+sd(x) & x > mean(x)-sd(x) ~ "Comparator",
+            x < mean(x)-sd(x) ~ "1 SD < Mean")
 
 Merged_PGS_PES %>% 
   ggplot() +
@@ -83,6 +128,22 @@ Merged_PGS_PES %>%
   xlab("Spot Na:K ratio (SD)") +
   ylab("SBP (mmHg)") +
   ggtitle("Main additive effects")
+
+## Other int plot
+
+Merged_PGS_PES$Na_binary <- ifelse(Merged_PGS_PES$Scaled_Na > 1, "Above", "Below")
+
+TE2 <- lm(Mean_SBP ~ Sex + Age + Age2 + Assesment_centre_month + 
+           Assesment_centre + PC1 + PC2 + PC3 + PC4 + PC5 + PC10 + PC11 +
+           PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + Na_binary*Transport_SBP_PES_AVG, data = Merged_PGS_PES)
+
+plot_model(TE2, type = "pred", terms = c("Na_binary", "Transport_SBP_PES_AVG"))
+
+TE3 <- lm(Mean_SBP ~ Sex + Age + Age2 + Assesment_centre_month + 
+            Assesment_centre + PC1 + PC2 + PC3 + PC4 + PC5 + PC10 + PC11 +
+            PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + Na_binary*SBP_PGS_Pt_0_01_AVG, data = Merged_PGS_PES)
+
+plot_model(TE3, type = "pred", terms = c("Na_binary", "SBP_PGS_Pt_0_01_AVG"))
 
 Merged_PGS_PES$Scaled_K <- as.numeric(scale(Merged_PGS_PES$K_urine_millimolL))
 
@@ -95,26 +156,26 @@ Merged_PGS_PES %>%
   ylab("SBP (mmHg)") +
   ggtitle("Main additive effects")
 
-## Calculate the effect of PES and PRS per decile of sodium
+## Calculate the effect of sodium per score decile
 
-Merged_PGS_PES$Decile_Na <- ntile(Merged_PGS_PES$Na_urine_millimolL, 10)
+Merged_PGS_PES$Decile_PES <- ntile(Merged_PGS_PES$Transport_SBP_PES_AVG, 10)
+Merged_PGS_PES$Decile_PGS <- ntile(Merged_PGS_PES$SBP_PGS_Pt_0_01_AVG, 10)
 
 
-Na_assoc_per_decile <- function(score, decile, Na_col, df) {
-  new_df <- df[df[[Na_col]] == decile,]
-  new_df$scaled_score <- as.numeric(scale(new_df[[score]]))
+Na_assoc_per_decile <- function(score, decile, df) {
+  new_df <- df[df[[score]] == decile,]
   mod <- lm(Mean_SBP ~ Sex + Age + Age2 + Assesment_centre_month + 
                                      Assesment_centre + PC1 + PC2 + PC3 + PC4 + PC5 + PC10 + PC11 +
-                                     PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + scaled_score, data = new_df)
+                                     PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + scale(Na_urine_millimolL), data = new_df)
   return(summary(mod))
 }
 
 Vec_decile <- c(1,2,3,4,5, 6, 7, 8, 9, 10)
 
-PES <- sapply(Vec_decile, Na_assoc_per_decile, Na_col = "Decile_Na",
-              df = Merged_PGS_PES, score = "Transport_SBP_PES_AVG")
-PRS <- sapply(Vec_decile, Na_assoc_per_decile, Na_col = "Decile_Na",
-              df = Merged_PGS_PES, score = "SBP_PGS_Pt_0_01_AVG")
+PES <- sapply(Vec_decile, Na_assoc_per_decile,
+              df = Merged_PGS_PES, score = "Decile_PES")
+PRS <- sapply(Vec_decile, Na_assoc_per_decile,
+              df = Merged_PGS_PES, score = "Decile_PGS")
 
 
 PES_extract <- apply(PES, 2, function(x) return(as.data.frame(x$coefficients)[53, 1:4]))
@@ -158,7 +219,7 @@ sapply(Val, Stat_compare_PES)
 sapply(Val, Stat_compare_PRS)
 
 
-## PES Na est per quintile
+## Na est per PES decile
 
 PES_plot$L_CI <- PES_plot$Estimate - (1.96*PES_plot$`Std. Error`)
 PES_plot$U_CI <- PES_plot$Estimate + (1.96*PES_plot$`Std. Error`)
@@ -167,25 +228,27 @@ mean(PES_plot$Estimate)
 mean(PES_plot$Estimate)-sd(PES_plot$Estimate)
 mean(PES_plot$Estimate)+sd(PES_plot$Estimate)
 
+
 P_P <- ggplot(PES_plot, aes(Val, Estimate)) +
-  geom_errorbar(
-    aes(ymin = L_CI, ymax = U_CI, color = as.factor(Val))) +
+  geom_errorbar(aes(ymin = L_CI, ymax = U_CI, color = as.factor(Val))) +
   geom_point(aes(color = as.factor(Val)), position = position_dodge(0.3)) +
-  stat_smooth(method = "loess", se = F, size = 0.5, colour = "black") +
+  #stat_smooth(method = "loess", se = F, size = 0.5, colour = "black") +
+  stat_smooth(method = "lm", se = F, size = 0.5, colour = "blue") +
   theme_bw() +
   theme(legend.position = "none") +
-  ylab("Effect on SBP (mmHg) per SD in score") +
-  xlab("Urinary Sodium (deciles)") +
-  geom_hline(yintercept = 0.47, lty = "dashed") +
-  ggtitle("Transport PES") +
+  ylab("Effect on SBP (mmHg) per SD in urinary Na+") +
+  xlab("Transport PES (deciles)") +
+  geom_hline(yintercept = 1.18338, lty = "dashed") +
+  ggtitle("Na+/K+ Transport PES") +
   ylim(0, 1.95) +
-  annotate("rect", xmin = -Inf, xmax = Inf, ymin = 0.6038735, ymax = Inf, fill = "grey", alpha = .2, color = NA) +
-  annotate("rect", xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = 0.3420018, fill = "grey", alpha = .2, color = NA)
+  annotate("rect", xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = 1.035152, fill = "grey", alpha = .2, color = NA) +
+  annotate("rect", xmin = -Inf, xmax = Inf, ymin = 1.331607, ymax = Inf, fill = "grey", alpha = .2, color = NA)
 
 PES_final <- P_P + scale_x_continuous(breaks = 1:10,
                          labels = paste(c("1st", "2nd", "3rd", "4th", 
-                                           "5th", "6th", "7th", "8th", "9th", "10th"), " Decile")) +
-  theme(axis.text.x = element_text(angle = 90))
+                                          "5th", "6th", "7th", "8th", "9th", "10th"), " Decile")) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  scale_color_aaas()
 
 ## PRS
 PRS_plot$Val <- c(1,2,3,4,5,6,7,8,9,10)
@@ -200,16 +263,17 @@ P_PRS <- ggplot(PRS_plot, aes(Val, Estimate)) +
   geom_errorbar(
     aes(ymin = L_CI, ymax = U_CI, color = as.factor(Val))) +
   geom_point(aes(color = as.factor(Val)), position = position_dodge(0.3)) +
-  stat_smooth(method = "loess", se = F, size = 0.5, colour = "black") +
+  stat_smooth(method = "lm", se = F, size = 0.5, colour = "blue") +
   theme_bw() +
   theme(legend.position = "none") +
   ylab(" ") +
-  xlab("Urinary Sodium (deciles)") +
-  geom_hline(yintercept = 1.606948, lty = "dashed") +
+  xlab("Genome-wide PGS (deciles)") +
+  geom_hline(yintercept = 1.186676, lty = "dashed") +
   ggtitle("Genome-wide PGS") +
   ylim(0, 1.95) +
-  annotate("rect", xmin = -Inf, xmax = Inf, ymin = 1.697952, ymax = Inf, fill = "grey", alpha = .2, color = NA) +
-  annotate("rect", xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = 1.515944, fill = "grey", alpha = .2, color = NA)
+  annotate("rect", xmin = -Inf, xmax = Inf, ymin = 1.305124, ymax = Inf, fill = "grey1", alpha = .2, color = NA) +
+  annotate("rect", xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = 1.068228, fill = "grey", alpha = .2, color = NA) +
+  scale_color_aaas()
 
 PRS_final <- P_PRS + scale_x_continuous(breaks = 1:10,
                                       labels = paste(c("1st", "2nd", "3rd", "4th", 
@@ -254,10 +318,37 @@ Variance_df <- rbind(Females_BP, Males_BP)
 Variance_df$PES_decile <- as.factor(ntile(Variance_df$Transport_SBP_PES_AVG, 10))
 Variance_df$PRS_decile <- as.factor(ntile(Variance_df$SBP_PGS_Pt_0_01_AVG, 10))
 
+Variance_df$PES_quartile <- as.factor(ntile(Variance_df$Transport_SBP_PES_AVG, 4))
+Variance_df$PRS_quartile <- as.factor(ntile(Variance_df$SBP_PGS_Pt_0_01_AVG, 4))
+
+Females_Na <- Pre_process("Na_urine_millimolL", "Scaled_Na_vQTL", "Female", "Sex", Merged_PGS_PES)
+Males_Na <- Pre_process("Na_urine_millimolL", "Scaled_Na_vQTL", "Male", "Sex", Merged_PGS_PES)
+
+
+Variance_df_2 <- rbind(Females_Na, Males_Na)
+
+Variance_df_2$PES_decile <- as.factor(ntile(Variance_df_2$Transport_SBP_PES_AVG, 10))
+Variance_df_2$PRS_decile <- as.factor(ntile(Variance_df_2$SBP_PGS_Pt_0_01_AVG, 10))
+Variance_df_2$PES_quartile <- as.factor(ntile(Variance_df_2$Transport_SBP_PES_AVG, 4))
+Variance_df_2$PRS_quartile <- as.factor(ntile(Variance_df_2$SBP_PGS_Pt_0_01_AVG, 4))
+
 ## Levene's test
 
 leveneTest(Scaled_SBP ~ as.factor(PES_decile), data = Variance_df)
+
 leveneTest(Scaled_SBP ~ as.factor(PRS_decile), data = Variance_df)
+
+leveneTest(Scaled_SBP ~ as.factor(PES_quartile), data = Variance_df)
+
+leveneTest(Scaled_SBP ~ as.factor(PRS_quartile), data = Variance_df)
+
+leveneTest(Scaled_Na_vQTL ~ as.factor(PES_decile), data = Variance_df_2)
+
+leveneTest(Scaled_Na_vQTL ~ as.factor(PRS_decile), data = Variance_df_2)
+
+leveneTest(Scaled_Na_vQTL ~ as.factor(PES_quartile), data = Variance_df_2)
+
+leveneTest(Scaled_Na_vQTL ~ as.factor(PRS_quartile), data = Variance_df_2)
 
 ## Some nominal evidence for non-homogeneous variance between PES deciles in terms of SBP, moreso in terms of PGS
 
